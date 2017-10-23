4aff807b465dfcedaca452935e9570e61e8d1ac4
Nathan Gray <nathan.gray@gordon.edu>
2017-10-23 11:53:08 -0400

    Removing some bootstrap styling, taking body and Html tags out of components. They belong in the index. Making user data in the user service persistent

--- assignment/app/services/user.service.js
+++ assignment/app/services/user.service.js
@@ -9,46 +9,66 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var USERS = [
-    { id: 123, userName: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice.wonder@gordon.edu" },
-    { id: 234, userName: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob.marley@gordon.edu" },
-    { id: 345, userName: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly.garcia@gordon.edu" },
-    { id: 456, userName: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi", email: "jose.annunzi@gordon.edu" }
-];
+var USERS = [];
 var UserService = (function () {
     function UserService() {
     }
+    UserService.prototype.ngOnInit = function () {
+        USERS = [
+            { id: 123, userName: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice.wonder@gordon.edu" },
+            { id: 234, userName: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob.marley@gordon.edu" },
+            { id: 345, userName: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly.garcia@gordon.edu" },
+            { id: 456, userName: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi", email: "jose.annunzi@gordon.edu" }
+        ];
+    };
     // Adds the user parameter instance to the local users array
     UserService.prototype.createUser = function (user) {
+        this.retrieveData();
         if (user.userName != null && user.password != null) {
             // generate a random id between 0 and 999 - should probably be next sequentially in some index of existing id's
             var id = Math.floor(Math.random() * 900) + 100;
             user.id = id;
             USERS.push(user);
         }
+        this.storeData();
         return user;
     };
     // Returns the user in local users array whose id matches the userId parameter
     UserService.prototype.findUserById = function (id) {
+        this.retrieveData();
         return USERS.find(function (user) { return user.id === 1 * id; });
     };
     // Returns the user in local users array whose username matches the parameter username
     UserService.prototype.findUserByUsername = function (userName) {
+        this.retrieveData();
         return USERS.find(function (user) { return user.userName === userName; });
     };
     // returns the user whose username and password match the username and password parameters
     UserService.prototype.findUserByCredentials = function (userName, password) {
+        this.retrieveData();
         return USERS.find(function (user) { return user.userName === userName; }, function (user) { return user.password === password; });
     };
     // updates the user in local users array whose id matches the userId parameter
     UserService.prototype.updateUser = function (id, user) {
+        this.retrieveData();
         var index = USERS.findIndex(function (user) { return user.id === id; });
         USERS[index] = user;
+        this.storeData();
     };
     // removes the user whose id matches the userId parameter
     UserService.prototype.deleteUser = function (id) {
+        this.retrieveData();
         var index = USERS.findIndex(function (user) { return user.id === id; });
         USERS.splice(index, 1);
+        this.storeData();
+    };
+    UserService.prototype.storeData = function () {
+        var storedUsers = JSON.stringify(USERS);
+        localStorage.setItem("storedUsers", storedUsers);
+    };
+    UserService.prototype.retrieveData = function () {
+        var retrievedUsers = localStorage.getItem("storedUsers");
+        USERS = JSON.parse(retrievedUsers);
     };
     return UserService;
 }());
--- assignment/app/services/user.service.ts
+++ assignment/app/services/user.service.ts
@@ -1,51 +1,74 @@
 import { Injectable } from '@angular/core'
 import { IUser } from '../components/user/user.model'
 
-var USERS: Array<IUser> = [
-  { id: 123, userName: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice.wonder@gordon.edu" },
-  { id: 234, userName: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob.marley@gordon.edu" },
-  { id: 345, userName: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly.garcia@gordon.edu" },
-  { id: 456, userName: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi", email: "jose.annunzi@gordon.edu" }
-];
+var USERS: Array<IUser> = [];
 
 @Injectable()
 export class UserService {
 
+  ngOnInit() {
+    USERS = [
+      { id: 123, userName: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice.wonder@gordon.edu" },
+      { id: 234, userName: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob.marley@gordon.edu" },
+      { id: 345, userName: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly.garcia@gordon.edu" },
+      { id: 456, userName: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi", email: "jose.annunzi@gordon.edu" }
+    ];
+  }
+
   // Adds the user parameter instance to the local users array
   createUser(user): IUser {
+    this.retrieveData();
     if (user.userName != null && user.password != null) {
       // generate a random id between 0 and 999 - should probably be next sequentially in some index of existing id's
       var id = Math.floor(Math.random() * 900) + 100;
       user.id = id;
       USERS.push(user);
     }
+    this.storeData();
     return user;
   }
 
   // Returns the user in local users array whose id matches the userId parameter
   findUserById(id): IUser {
+    this.retrieveData();
     return USERS.find(user => user.id === 1 * id);
   }
 
   // Returns the user in local users array whose username matches the parameter username
   findUserByUsername(userName): IUser {
+    this.retrieveData();
     return USERS.find(user => user.userName === userName);
   }
 
   // returns the user whose username and password match the username and password parameters
   findUserByCredentials(userName, password): IUser {
+    this.retrieveData();
     return USERS.find(user => user.userName === userName, user => user.password === password);
   }
 
   // updates the user in local users array whose id matches the userId parameter
   updateUser(id, user) {
+    this.retrieveData();
     var index = USERS.findIndex(user => user.id === id);
     USERS[index] = user;
+    this.storeData();
   }
 
   // removes the user whose id matches the userId parameter
   deleteUser(id) {
+    this.retrieveData();
     var index = USERS.findIndex(user => user.id === id);
     USERS.splice(index, 1);
+    this.storeData();
+  }
+
+  storeData() {
+    var storedUsers = JSON.stringify(USERS);
+    localStorage.setItem("storedUsers", storedUsers);
+  }
+
+  retrieveData() {
+    var retrievedUsers = localStorage.getItem("storedUsers");
+    USERS = JSON.parse(retrievedUsers);
   }
 }    

114d3c3af45c6ab2bf8a10a99cb3cafe5c636fb2
Nathan Gray <nathan.gray@gordon.edu>
2017-10-31 01:43:21 -0400

    Adding user controller, user model, and first http request

---
+++ assignment/WebApi/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+  [Route("api/[controller]")]
+  public class UserController : Controller
+  {
+    IUser[] USERS = new IUser[]
+    {
+      new IUser(123, "alice", "alice", "Alice", "Wonder", "alice.wonder@gordon.edu"),
+      new IUser(234, "bob", "bob", "Bob", "Marley", "bob.marley@gordon.edu"),
+      new IUser(345, "charly", "charly", "Charly", "Garcia", "charly.garcia@gordon.edu"),
+      new IUser(456, "jannunzi", "jannunzi", "Jose", "Annunzi", "jose.annunzi@gordon.edu")
+    };
+
+    // GET api/user
+    [HttpGet]
+    public IUser[] Get()
+    {
+      return USERS;
+    }
+
+    // // GET api/values/5
+    // [HttpGet("{id}")]
+    // public string Get(int id)
+    // {
+    //     return "value";
+    // }
+
+    // // POST api/values
+    // [HttpPost]
+    // public void Post([FromBody]string value)
+    // {
+    // }
+
+    // // PUT api/values/5
+    // [HttpPut("{id}")]
+    // public void Put(int id, [FromBody]string value)
+    // {
+    // }
+
+    // // DELETE api/values/5
+    // [HttpDelete("{id}")]
+    // public void Delete(int id)
+    // {
+    // }
+  }
+}
---
+++ assignment/WebApi/Models/UserModel.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+  public class IUser
+  {
+    public IUser(string UserName, string Password)
+    {
+      this.Id = GenerateId();
+      this.UserName = UserName;
+      this.Password = Password;
+    }
+
+    public IUser(int Id, string UserName, string Password, string FirstName, string LastName, string Email)
+    {
+      this.Id = Id;
+      this.UserName = UserName;
+      this.Password = Password;
+      this.FirstName = FirstName;
+      this.LastName = LastName;
+      this.Email = Email;
+    }
+
+    private int GenerateId()
+    {
+      // Generate a number between 0 and 999
+      return new Random().Next(0, 999);
+    }
+
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+  }
+}

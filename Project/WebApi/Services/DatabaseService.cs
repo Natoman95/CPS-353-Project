674e5c799784c885646b223434bc7a1af13918e7
Nathan Gray <nathan.gray@gordon.edu>
2017-12-15 15:40:24 -0500

    Moving database init dummy data to database service

--- Project/WebApi/Services/DatabaseService.cs
+++ Project/WebApi/Services/DatabaseService.cs
@@ -28,6 +28,28 @@ namespace WebApi.Services
       }
     }
 
+    // Initialize Database service to have dummy data for the website to use
+    public void InitializeDatabase()
+    {
+      // Ids that index with the creation of any department or user
+      DatabaseService.Instance.CurrentDepartmentId = 1;
+      DatabaseService.Instance.CurrentUserId = 1;
+
+      // Users to log in with and manipulate
+      Student student1 = new Student("BMarl", "password123", "bob.marley@gordon.edu", "Bob", "Marley");
+      Scholar scholar1 = new Scholar("Bloggo_doggo", "yoyo@97", "joe.bloggs@gordon.edu", "Joe", "Bloggs");
+      Institution inst1 = new Institution("GoCo", "livesworthleading1889", "gordon@gordon.edu", "Gordon College");
+
+      // Add some departments to the institution
+      inst1.AddDepartment(new Department("Biology", inst1));
+      inst1.AddDepartment(new Department("History", inst1));
+      inst1.AddDepartment(new Department("Philosophy", inst1));
+
+      DatabaseService.Instance.Users.Add(student1);
+      DatabaseService.Instance.Users.Add(scholar1);
+      DatabaseService.Instance.Users.Add(inst1);
+    }
+
     public long CurrentUserId
     {
       get
--- Project/WebApi/Startup.cs
+++ Project/WebApi/Startup.cs
@@ -19,26 +19,7 @@ namespace WebApi
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
-
-      // Initialize Database service to have dummy data for the website to use
-
-      // Ids that index with the creation of any department or user
-      DatabaseService.Instance.CurrentDepartmentId = 1;
-      DatabaseService.Instance.CurrentUserId = 1;
-
-      // Users to log in with and manipulate
-      Student student1 = new Student("BMarl", "password123", "bob.marley@gordon.edu", "Bob", "Marley");
-      Scholar scholar1 = new Scholar("Bloggo_doggo", "yoyo@97", "joe.bloggs@gordon.edu", "Joe", "Bloggs");
-      Institution inst1 = new Institution("GoCo", "livesworthleading1889", "gordon@gordon.edu", "Gordon College");
-
-      // Add some departments to the institution
-      inst1.AddDepartment(new Department("Biology", inst1));
-      inst1.AddDepartment(new Department("History", inst1));
-      inst1.AddDepartment(new Department("Philosophy", inst1));
-
-      DatabaseService.Instance.Users.Add(student1);
-      DatabaseService.Instance.Users.Add(scholar1);
-      DatabaseService.Instance.Users.Add(inst1);
+      DatabaseService.Instance.InitializeDatabase();
     }
 
     public IConfiguration Configuration { get; }
